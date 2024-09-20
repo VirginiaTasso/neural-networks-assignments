015e975e13574b59f94286a697410ece59d89033
Virginia Tasso <virginiatasso01@gmail.com>
2024-09-20 17:20:14 +0200

    started hw 4

---
+++ hw4/hw4:TASSO.py
@@ -0,0 +1,9 @@
+# --------- Importing useful libraries ---------
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+import pandas as pd
+
+# --------- Load Dataset ---------
+mnist = load_digits()
